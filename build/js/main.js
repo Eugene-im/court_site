f9c8c90cf57fe87beaa1eacb3e7d4950e7d5abd7
Eugene-im <em.92@mail.ru>
2019-05-20 12:44:52 +0300

    add submenu in side block

--- build/js/main.js
+++ build/js/main.js
@@ -42,6 +42,27 @@ $('.global__overlay,.header__menu__top_h .header__logo').click(function () {
     $('.header__menu__side_h').hide();
 })
 
+
+var conf_sm="#sm1,#sm2,#sm3,#sm4,#sm5"
+var conf_smxs="#sm1s,#sm2s,#sm3s,#sm4s,#sm5s"
+
+$(conf_sm).hover(function(){
+    $(conf_smxs).css('display','none');
+    var x = this.id + "s";
+    $('#'+x).css('display','flex');
+})
+
+var conf_submSide = '#submenu1,#submenu2,#submenu3,#submenu4,#submenu5,#submenu6';
+var conf_topS = '.sub1,.sub2,.sub3,.sub4,.sub5,.sub6';
+
+$(conf_topS).click(function(e){
+    e.preventDefault();
+    $(conf_submSide).css('display','none');
+    $('#topmenu').css('display','none');
+    var x = $(this).attr('class').substr(-1);   
+    $('#submenu'+x).css('display','block')
+})
+
 const conf = '.obl__3,.obl__0,.obl__2,.obl__1';
 const conf2 = '.logo__3,.logo__0,.logo__2,.logo__1';
 const color1 = '#3D5798';
